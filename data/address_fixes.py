b821fe036e5b893a5c89529847450556194f0d75
Nick Kullman <Nick.Kullman@gmail.com>
2020-12-15 19:10:59 -0800

    Delete old data objects

--- data/address_fixes.py
+++
@@ -1,7 +0,0 @@
-"""A mapping from original addresses to geocode-able addresses."""
-
-
-addr_mappings = {
-    "520 2nd Ave W #406, Seattle, WA 98119": "520 2nd Ave W, Seattle, WA 98119",
-    "821 2nd Ave #402, Seattle, WA 98104": "821 2nd Ave, Seattle, WA 98104",
-}
