5bd7606926bb435671ff3c7be898fef7e505074d
Nick Kullman <Nick.Kullman@gmail.com>
2020-12-15 19:19:41 -0800

    Major overhauls for simplicity, applicability
    - Changes to accommodate new input data format
    - Better address parsing
    - Maintaining known customer info on the fly
    - Maintaining known distances on the fly

--- gocheche/runner.py
+++ gocheche/runner.py
@@ -13,22 +13,36 @@ def get_arg_parser() -> argparse.ArgumentParser:
     # Initialize an argument parser.
     parser = argparse.ArgumentParser(description="Does some CheChe routing.")
 
-    # Argument to take in the file with customer info (namely, addresses).
+    # Argument to take in the file with the list of customers to visit.
+    parser.add_argument(
+        'visit',
+        type=str,
+        help='Name of file with info for customers that need to be visited.'
+    )
+    
+    # Argument to specify where to write solution.
+    parser.add_argument(
+        "-o",
+        "--output",
+        type = str,
+        default = "solution.txt",
+        help = (
+            "Name of file to which to write solution. "
+            "Default is 'solution.txt'"
+        )
+    )
+
+    # Argument to take in the file with data on known customers.
     parser.add_argument(
         '-c',
         '--customers',
         type=str,
-        default="data/customers.json",
-        help='Filename with customer info'
-    )
-
-    # Argument to take in the file with the list of customers to visit.
-    parser.add_argument(
-        '-v',
-        '--visit',
-        type=str,
-        default="data/visit.json",
-        help='Filename with the customers to be visited'
+        default="data/customers_db.json",
+        help=(
+            'Name of file with info on known customers. '
+            "Default is customers_db.json. If the file doesn't exist, a new file is created "
+            "to start saving customer info."
+        )
     )
     
     # Argument to take in the file with routing constraints (namely, which
@@ -37,43 +51,8 @@ def get_arg_parser() -> argparse.ArgumentParser:
         '-p',
         '--params',
         type=str,
-        default="data/run_params.json",
+        default="",
         help='File with run paramaters (e.g., constraints)'
-    )
-    
-    # Argument to take in the file with the distance (duration) matrix.
-    parser.add_argument(
-        '-d',
-        '--distances',
-        type=str,
-        default="data/distances.json",
-        help='Distance matrix file'
-    )
-
-    parser.add_argument(
-        '--get-distances',
-        action="store_true",
-        help="Whether to fetch a new distance matrix if any customers are missing in the distances file."
-    )
-
-    # Argument to determine whether to write the solution to file.
-    parser.add_argument(
-        "-w",
-        "--write",
-        action="store_true",
-        help="Write output to file (specify name with -o)"
-    )
-
-    # Argument to specify where to write solution.
-    parser.add_argument(
-        "-o",
-        "--output",
-        type = str,
-        default = "data/solution.json",
-        help = (
-            "Name of file to which to write solution. "
-            "Default is '../data/solution.json'"
-        )
     )
 
     return parser
@@ -83,34 +62,13 @@ def fetch_data(args: argparse.Namespace) -> Tuple[List[str], Dict[str, Customer]
     """Fetches the data in the files located in the locations indicated by `args`."""
 
     # Loading all data files. We first start with the list of customers to visit
-    visits = utils.load_visits(args.visit)
-    logging.info("List of customers to visit retrieved.")
-    # Next we load all customer details.
-    customers = utils.load_customers(args.customers, to_visit=visits)
-    logging.info("Customer set retrieved.")
-    # Then the distances.
-    distances = utils.load_distances(args.distances)
-    logging.info("Distances retrieved.")
+    customers, visits, distances = utils.load_customers(args.visit, args.customers)
+    logging.info("Customers and distances retrieved.")
+    
     # And lastly the run parameters (constraints).
     params = utils.load_params(args.params)
     logging.info("Constraints file loaded.")
     
-    # Make sure our customers file contains all those we're trying to visit.
-    utils._ensure_no_missing_customers(visits, customers)
-    logging.info("Confirmed that all required customers are in the customers file.")
-
-    # Making sure our distances file contains distances for all customer pairs.
-    are_missing_distances = utils._check_for_missing_distances(visits, distances)
-    if are_missing_distances:
-        # If any distances are missing, we'll either fetch a new distances matrix.
-        # or just raise an error; whichever the user specified in args.
-        logging.info("Some distances were missing from the distances file.")
-        if args.get_distances:
-            logging.info("Fetching a new distance matrix for the customer set...")
-            distances = utils.get_distances(customers, args.distances)
-        else:
-            utils._find_missing_distances(visits, distances)
-
     return visits, customers, distances, params
 
 
@@ -127,10 +85,6 @@ def main():
     parser = get_arg_parser()
     args = parser.parse_args()
 
-    # If we don't want to write the output, then set the output file to None.
-    if not args.write:
-        args.output = None
-
     # Fetch and check the data in the files given in args.
     visits, customers, distances, params = fetch_data(args)
 
@@ -139,12 +93,10 @@ def main():
 
         Received arguments:
 
-            customers: {args.customers}
             visit: {args.visit}
-            params: {args.params}
-            distances: {args.distances}
-            write: {args.write}
             output: {args.output}
+            customers: {args.customers}
+            params: {args.params}
 
         Customers to visit:
 
--- gocheche/utils.py
+++ gocheche/utils.py
@@ -1,25 +1,23 @@
 from typing import Any, Dict, List, Optional, Tuple
 import ast
+import csv
 import json
 import logging
+import os
 
 from openrouteservice import client, directions, distance_matrix
 import geocoder
+import usaddress
 
-from data import address_fixes, api_key
+from data import api_key
 from gocheche.core import Customer, RunParams
+
 
 DEPOT_CUST_ID = "000000"
 OSR_API_KEY = api_key.MY_OSR_API_KEY
+NAME_COL_IDX = 0
+ADDRESS_COL_IDX = 6
 
-
-def groom_address(address: str) -> str:
-    """Makes an address amenable to geocoding."""
-
-    # TODO currently just using a manual mapping, but regex or something similar
-    # would be more robust.
-    return address_fixes.addr_mappings.get(address, address)
-    
 
 def get_latlon(address: str) -> Tuple[float, float]:
     """Retrieves (latitude, longitude) for the given customer."""
@@ -30,9 +28,8 @@ def get_latlon(address: str) -> Tuple[float, float]:
         )
 
     logging.info(f"Geocoding the following address:\n\t{address}")
-    groomed_address = groom_address(address)
     
-    g = geocoder.osm(groomed_address)
+    g = geocoder.osm(address)
 
     return (g.json['lat'], g.json['lng'])
 
@@ -51,67 +48,198 @@ def write_json(obj_to_write: Any, filename: str):
         json.dump(obj_to_write, json_file, indent=4)
 
 
-def load_visits(visits_filename: str) -> List:
-    """Loads the file of customer IDs to visit into a *sorted* list."""
+def parse_address(address: str) -> str:
+    """Provides a geocodable version of `address`."""
     
-    visits = load_json(visits_filename)['visit']
-    visits.insert(0, DEPOT_CUST_ID)
-    # ^^ NOTE: Other methods (router.create_model_data) assume depot is in the first slot.
-    # Beware of changing this in the future.
-    if len(visits) != len(set(visits)):
-        return ValueError("Some customers are duplicated in the visits file.")
-    return sorted(visits)
-
-
-def load_customers(customers_filename: str, to_visit: List[str], keep_all: bool = False, refresh_latlon: bool = False) -> Dict[str, Customer]:
-    """Loads the address file into a dict of customers, keyed on their cust_id.
+    parsed, addr_type = usaddress.tag(address)
+    if addr_type != "Street Address":
+        raise ValueError(f"Address could not be properly parsed. Resulting type: {addr_type}. Result: \n{parsed}")
     
-    Inputs:
-        to_visit: Subset of customers that need to be visited (should be a list of cust_ids).
+    # Trim off any whitespace
+    for part in parsed:
+        parsed[part] = parsed[part].strip()
+    
+    # Initialize the result with the address (aka house/street) number
+    result = parsed['AddressNumber']
+    
+    # Get the whole street name
+    for k, v in [(k, v) for k, v in parsed.items() if k.startswith("StreetName")]:
+        result += f" {v}"
+    
+    # Add the city, state, and zip.
+    result += f", {parsed['PlaceName']}, {parsed['StateName']} {parsed['ZipCode']}"
+    
+    # Done.
+    return result
 
-        refresh_latlon: Whether to refresh all customers' lat/lon coords (by default we only refresh
-            those that are missing).
 
-        keep_all: Whether to return ALL customers (including those that don't need to visited).
-            
+def get_known_customer(name: str, address: str, customers: List[Customer], visit: bool=True) -> Optional[Customer]:
+    """If customers contains a Customer with the given name and address, returns
+    that customer, with its visit field set to the value of `visit`.
     """
 
-    customers = load_json(customers_filename)['customers']
-    updated = False
     for customer in customers:
-        if (
-            refresh_latlon
-            or customer.get('latitude', None) is None
-            or customer.get('longitude', None) is None
-        ):
-            logging.info(f"Updating customer's lat/lon coords: {customer}")
-            updated = True
-            lat, lon = get_latlon(customer['address'])
-            customer['latitude'] = lat
-            customer['longitude'] = lon
-            customer['visit'] = customer['cust_id'] in to_visit
+        if customer.name == name and customer.address == address:
+            customer.visit = visit
+            return customer
     
-    if updated:
-        logging.info(f"Writing an updated customers file: {customers_filename}")
-        to_write = {
-            'customers': [
-                {k:v for k,v in customer.items() if k != 'visit'} # Don't want to write the to-visit field to file
-                for customer in customers
-            ]
-        }
-        write_json(to_write, customers_filename)
+    # No such customer found.
+    return None
 
-    if keep_all:
-        return {
-            customer['cust_id']: Customer(**customer)
-            for customer in cutomers
-        }
+
+def load_known_customer_data(customers_filename: str) -> Tuple[List[Customer], Dict[Tuple[str, str], float]]:
+    """Loads the list of known customers from customers_filename."""
+
+    if not customers_filename or not os.path.exists(customers_filename):
+        # empty list of customers, empty dict of distances
+        return [], {}
+
     else:
-        return {
-            customer['cust_id']: Customer(**customer)
-            for customer in customers
-            if customer['cust_id'] in to_visit
-        }
+        all_cust_data = load_json(customers_filename)
+        custs = [Customer(**customer) for customer in all_cust_data['customers']]
+        dists = dist_dict_from_json(all_cust_data['distances'])
+        return custs, dists
+
+
+def load_customers(
+    visits_filename: str,
+    customers_filename: Optional[str],
+) -> Tuple[Dict[str, Customer], List, Dict[Tuple[str, str], float]]:
+    """Loads customer-related data objects from file:
+        element 0: dict from customer IDs to customer objects
+        element 1: *sorted* list of customer IDs (depot first) that need to be visited
+        element 2: distances matrix
+
+    The visits_filename points to a CSV file with a row for each customer that needs to be visited
+        in the current route. Must have columns for customers' names and addresses.
+
+    The customers_filename points to a JSON file that contains a list of known customers, as well as
+    the distances between them.
+        If the filename is None or empty, we do not read any customer info from file.
+        If a filename is provided but it does not exist, we write a new file containing
+            the customers in visits_filename, along with the distances between them.
+        If the file exists and any customers in visits_filename are not already in this file,
+            then we add them to the file, along with their distances.
+
+    """
+
+    # Read in the file that contains known customers and their details.
+    known_customers, distances_dict = load_known_customer_data(customers_filename)
+    
+    # Initialize the dict of current customers and the list of customer IDs to visit
+    cust_dict = {}
+    visits = [] 
+
+    # Note what will be the next ID that we assign to a customer if we encounter
+    # a new one.
+    # If the list of customers is empty, then we start at 0.
+    next_cust_id = (
+        0 if not known_customers
+        else max(int(customer.cust_id) for customer in known_customers) + 1
+    )
+
+    # Read in the CSV that contains the details of the customers to be visited
+    with open(visits_filename) as csvfile:
+        rowreader = csv.reader(csvfile)
+        for row in rowreader:
+
+            # Read its information from the row
+            name = row[NAME_COL_IDX]
+            address = parse_address(row[ADDRESS_COL_IDX])
+
+            # It's in the visits file, so we know it needs to be visited.
+            visit = True
+            
+            cust = get_known_customer(name, address, known_customers)
+            
+            if cust is None:
+                # We're dealing with a new customer
+                logging.info(f"Found new customer: {name}, located at:\n{address}")
+                
+                # Get a new ID for the customer
+                cust_id = f"{next_cust_id:06d}"
+                next_cust_id += 1
+                logging.info(f"{name} given customer ID {cust_id}")
+                
+                # Get the customer's lat/long
+                lat, lon = get_latlon(address)
+                logging.info(f"Lat/lon found for {name}.")
+
+                # Create a Customer object for this customer
+                cust = Customer(cust_id, name, address, lat, lon, True)
+
+                # Get the distances to/from this new customer from/to the known customers
+                new_dists = get_dists_to_from_new_cust(cust, known_customers)
+                logging.info(f"Distances for {name} retrieved.")
+                
+                # Write this new customer to the customer file
+                add_to_known_customer_data(cust, new_dists, customers_filename)
+
+                # Add this customer to our list of known customers
+                known_customers.append(cust)
+
+                # Add these new-customer distances to our distances dict
+                distances_dict.update(new_dists)
+            
+            else:
+                logging.info(f"Loading existing customer:\n{cust.out_dict}")
+
+            cust_dict[cust.cust_id] = cust
+
+            visits.append(cust.cust_id)
+
+    # Sort the list of customers to visit by their IDs.
+    visits = sorted(visits)
+
+    # If the depot is not already included in the list of customers to visit, add it in now
+    if DEPOT_CUST_ID not in visits:
+        visits.insert(0, DEPOT_CUST_ID)
+    # ^^ NOTE: Other methods (router.create_model_data) assume depot is in the first slot.
+    # Beware of changing this in the future.
+    
+    if len(visits) != len(set(visits)):
+        return ValueError("Some customers are duplicated in the visits file.")
+    
+    logging.info(f"IDs of customers to be visited: {visits}")
+    
+    return cust_dict, visits, distances_dict
+
+
+def add_to_known_customer_data(
+    customer: Customer,
+    new_distances: Dict[Tuple[str, str], float],
+    customers_filename: Optional[str],
+):
+
+    # If we're trying to read/write from memory, nothing to do
+    if not customers_filename:
+        return
+        
+    # If a file was specified but doesn't yet exist, create an empty one
+    if not file_exists(customers_filename):
+        new_cust_file_json = {"customers":[], "distances":{}}
+        write_json(new_cust_file_json, customers_filename)
+
+    # Load the known customer data from file
+    cust_file_json = load_json(customers_filename)
+
+    # Add the customer to our known customer data
+    custs_data = cust_file_json["customers"]
+    custs_data.append({
+        "cust_id": customer.cust_id,
+        "name": customer.name,
+        "address": customer.address,
+        "latitude": customer.lat,
+        "longitude": customer.lon
+    })
+
+    # Add the new distances to our distance matrix
+    dists_data = dist_dict_from_json(cust_file_json["distances"])
+    dists_data.update(new_distances)
+
+    # Write the new data back to file
+    output_json = {"customers": custs_data, "distances": dist_dict_to_json(dists_data)}
+    write_json(output_json, customers_filename)
 
 
 def dist_dict_from_json(json_dists: Dict[str, float]) -> Dict[Tuple[str, str], float]:
@@ -130,105 +258,82 @@ def dist_dict_to_json(dist_dict: Dict[Tuple[str, str], float]) -> Dict[str, floa
     return {str(custs_key): dist for custs_key, dist in dist_dict.items()}
 
 
-def load_distances(distances_filename: str) -> Dict[Tuple[str, str], float]:
-    """Loads the distances file into a dict of distances, keyed on (origin, destination) tuples,
-    where origin and destination are customers' cust_ids.
-    """
+def file_exists(filename: str) -> bool:
+    """Checks whether the specified file exists."""
 
-    raw_distances_dict = load_json(distances_filename)['distances']
-    distances_dict = dist_dict_from_json(raw_distances_dict)
-    return distances_dict
+    return os.path.exists(filename)
 
 
 def load_params(params_filename: str) -> Dict:
     """Loads parameters and constraints from file."""
+    
+    # If no params filename is specified, return the default parameter setting.
+    if not params_filename:
+        return RunParams()
+
     return RunParams(**load_json(params_filename))
 
 
-def _ensure_no_missing_customers(visits: List[str], customers: Dict[str, Customer]):
-    """Raises a ValueError if any IDs on the list of customers to visit are missing from the customers file."""
-    missing_customers = [cust_id for cust_id in visits if cust_id not in {customer.cust_id for customer in customers.values()}]
-    if missing_customers:
-        raise ValueError(f"Not all to-visit customers are listed in the customer file:\n\t{missing_customers}")
+def get_dists_to_from_new_cust(
+    customer: Customer,
+    known_customers: List[Customer],
+) -> Dict[Tuple[str, str], float]:
+    """Gets distances to/from a new customer from/to the list of known customers.
 
+    Args:
+        customer: the new customer
+        known_customers: known customers
 
-def _find_missing_distances(visits: List[str], distances: Dict[Tuple[str, str], float]):
-    """Raises a ValueError if any customer pairs are missing from the distances file."""
-
-    missing_distance_pairs = []
-    for i, cust_1 in enumerate(visits):
-        for cust_2 in visits[i+1: ]:
-            if (cust_1, cust_2) not in distances:
-                missing_distance_pairs.append((cust_1, cust_2))
-            if (cust_2, cust_1) not in distances:
-                missing_distance_pairs.append((cust_2, cust_1))
-    if len(missing_distance_pairs) > 0:
-        raise ValueError(f"Not all pairs of the to-visit customers are covered by the distances file:\n\t{missing_distance_pairs}")
-
-
-def _check_for_missing_distances(visits: List[str], distances: Dict[Tuple[str, str], float]) -> bool:
-    """Specifies whether any customer pairs are missing from the distances file."""
-
-    for i, cust_1 in enumerate(visits):
-        for cust_2 in visits[i+1: ]:
-            if (cust_1, cust_2) not in distances or (cust_2, cust_1) not in distances:
-                return True
-    return False
-
-
-def get_distances(customers: Dict[str, Customer], filename: Optional[str] = None) -> Dict[Tuple[str, str], float]:
-    """Uses OpenRouteService to compute the distance matrix.
-
-    For each pair of customers in `customers`, computes the distance of the shortest route between them.
-
-    Inputs:
-
-        customers: The list of customers (importantly, with lat/lon coords) for which to get the
-            distance matrix
-
-        filename: The name of the file to which to write the resulting distance matrix.
-
-    Outputs:
-
-        The distance matrix as a dict, where a key is a tuple (i,j) of customer IDs, and its value
-            is the distance from i to j.
-
+    Returns:
+        Distances dictionary with pairwise distances between the new and known customers.
     """
+
+    # If there are no existing customers, just return a 0-distance dict for the
+    # single self-directed arc.
+    if not known_customers:
+        return {(customer.cust_id, customer.cust_id): 0.0}
 
     # Instantiate our client to pull the distance matrix.
     osr_client = client.Client(key=OSR_API_KEY)
 
-    # Get the list of customer coordinates that we want to get the distance matrix for, along with
-    # their IDs.
+    # Get the list of customer coordinates, along with their IDs.
     # Note that they're in (lon, lat) order instead of (lat, lon).
-    cust_coords_w_id = [(customer.cust_id, (customer.lon, customer.lat)) for customer in customers.values()]
+    cust_coords_w_id = [(known_cust.cust_id, (known_cust.lon, known_cust.lat)) for known_cust in known_customers]
 
-    # Number of customers.
+    # Append the new customer.
+    cust_coords_w_id.append((customer.cust_id, (customer.lon, customer.lat)))
+
+    # Note the number of total customers, along with the index of the new customer that we need distances for.
     n = len(cust_coords_w_id)
-    
-    # Define the request we'll make to fetch the distance matrix.
+    new_cust_idx = n-1
+
+    # Define the basic request we'll make to fetch the distance matrix.
     request = {
         'locations': [cust_coords[1] for cust_coords in cust_coords_w_id],
         'profile': 'driving-car',
         'metrics': ['duration'],
     }
-    
-    # Get the matrix
-    distance_matrix = osr_client.distance_matrix(**request)['durations']
-    logging.info(f"Distance matrix retrieved ({n} x {n}).")
-    print(distance_matrix)
 
-    # Convert it to the format we want to save it in.
-    distance_dict = {
-        (cust_coords_w_id[i][0], cust_coords_w_id[j][0]): distance_matrix[i][j]
-        for i in range(n) for j in range(n) 
+    # First, get the distances FROM the new customer to all the existing customers
+    request['sources'] = [new_cust_idx]
+    response = osr_client.distance_matrix(**request)['durations']
+    logging.info("Distances *FROM* new customer retrieved")
+    new_distances = {
+        (cust_coords_w_id[new_cust_idx][0], cust_coords_w_id[i][0]): response[0][i]
+        for i in range(n) # This also includes the distance from the new customer to itself
     }
 
-    if filename:
-        writable_dict = dist_dict_to_json(distance_dict)
-        write_json({'distances': writable_dict}, filename)
-
-    return distance_dict
+    # Next, get the distances TO the new customer from all the existing customers
+    request.pop('sources')                    # no longer want to specify a single source
+    request['destinations'] = [new_cust_idx]  # but we do want to specify a destination
+    response = osr_client.distance_matrix(**request)['durations']
+    logging.info("Distances *TO* new customer retrieved")
+    new_distances.update({
+        (cust_coords_w_id[i][0], cust_coords_w_id[new_cust_idx][0]): response[i][0]
+        for i in range(new_cust_idx) # don't include the dist to itself this time
+    })
+    
+    return new_distances
 
 
 def get_distance_matrix(visits: List[str], distances: Dict[Tuple[str, str], float]) -> List[List[float]]:
@@ -259,8 +364,8 @@ def get_route_geojson(route: List[Customer]):
 
 
 def stringify_route(route:[List[Customer]]) -> str:
-    strd_route = [cust.out_dict for cust in route]
-    return strd_route
+    stringified_route = [cust.out_dict for cust in route]
+    return stringified_route
 
 
 def get_center_of_custs(visits: List[str], customers: Dict[str, Customer]) -> Tuple[float, float]:
